a64beb13b23b1833a67696ac2bbcf45942503d3e
Denis Levashov <denis.ne@e1.ru>
2022-12-21 21:30:28 +0500

    решение третьей  задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+    //a = 25; b = 5 -> да
+    //a = 2 b = 10 -> нет
+    //a = 9; b = -3 -> да
+    //a = -3 b = 9 -> нет
+
+using static System.Console;
+WriteLine("два числа и проверяет, является ли первое число квадратом второго.");
+
+WriteLine("Введите число a ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите число b ");
+int b = int.Parse(ReadLine()!);
+
+if  (b*b ==  a)
+{
+    WriteLine($"Число {a} является квадратом числа {b}");
+}
+else
+    WriteLine($"Число {a} не является квадратом числа {b}");
