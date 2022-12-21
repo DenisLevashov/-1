9c82f23bf2cc7becf75f29904a4daf1777cdd556
Denis Levashov <denis.ne@e1.ru>
2022-12-21 23:41:33 +0500

    решение 8 задачи

---
+++ Homework1/Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+
+using static System.Console;
+Write("Введите положительное число: ");
+int a = int.Parse(ReadLine()!);
+int count = 2;
+while(count <= a)
+{
+    Write(count + " ");
+    count = count + 2;
+}
