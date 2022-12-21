88c6eda75717646bf94b8ff39d47a868547c6bbb
Denis Levashov <denis.ne@e1.ru>
2022-12-21 23:27:50 +0500

    решение 4 задачи

--- Homework1/Task4/Program.cs
+++ Homework1/Task4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+using static System.Console;
+
+Write("Введите число a ");
+int a = int.Parse(ReadLine()!);
+
+Write("Введите число b ");
+int b = int.Parse(ReadLine()!);
+
+Write("Введите число c ");
+int c = int.Parse(ReadLine()!);
+int max = a;
+
+if(b > max ) max = b;
+if(c > max ) max = c;
+WriteLine($"Максимальное число {max}");
