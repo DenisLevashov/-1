a16a7b6f7429f67f711e7f12ceaa43ec1f610a13
Denis Levashov <denis.ne@e1.ru>
2023-01-10 23:00:32 +0500

    задача 19

---
+++ Task19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+using static System.Console;
+
+Clear();
+
+Write("Задайте координаты точки A, по оси X: ");
+int.TryParse(ReadLine(),out int Ax);
+Write("Задайте координаты точки A, по оси Y: ");
+int.TryParse(ReadLine(),out int Ay);
+Write("Задайте координаты точки B, по оси X: ");
+int.TryParse(ReadLine(),out int Bx);
+Write("Задайте координаты точки B, по оси Y: ");
+int.TryParse(ReadLine(),out int By);
+
+double pif = Math.Sqrt((Bx - Ax)*(Bx - Ax) + (By - Ay)*(By - Ay));
+double result = pif;
+WriteLine(result);
