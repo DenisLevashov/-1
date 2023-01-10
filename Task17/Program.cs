f7b5a78acdbfcb2842a0ce2fcb1e40606dc4fadf
Denis Levashov <denis.ne@e1.ru>
2023-01-10 22:23:19 +0500

    решение 18 задачи, добавил услвоие

---
+++ Task17/Program.cs
@@ -0,0 +1,68 @@
+﻿//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+using static System.Console;
+
+Clear();
+
+Write("Задайте номер четверти: ");
+
+
+bool isParse=int.TryParse(ReadLine(),out int i);
+if(!isParse)
+{
+    WriteLine("Введено не число");
+    return;
+}
+
+switch(i)
+{
+    case 1:
+    {
+        WriteLine("x>0,y>0");
+        break;
+    }
+    case 2:
+    {
+        WriteLine("x<0,y>0");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("x<0,y<0");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("x>0,y<0");
+        break;
+    }
+    default:
+    {
+        WriteLine("читай внимательно условие");
+        break;
+    }
+
+    
+}
+
+// if(i==1)
+// {
+//     WriteLine("x>0,y>0");
+// }
+// if(i==2)
+// {
+//     WriteLine("x<0,y>0");
+// }
+// if(i==3)
+// {
+//     WriteLine("x<0,y<0");
+// }
+// if(i==4)
+// {
+//     WriteLine("x>0,y<0");
+// }
+// if(i>4 || i<1)
+// {
+//     WriteLine("читай внимательно условие");
+// }
