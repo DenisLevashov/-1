1860978d06adfb8a9b75de5dec6645f925613550
Denis Levashov <denis.ne@e1.ru>
2023-01-13 21:15:33 +0500

    решение22 задачи

---
+++ Task22/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+
+using static System.Console;
+Clear();
+
+Write("Введите число: " );
+int number=int.Parse(ReadLine()!);
+int sum=NumCount(number);
+WriteLine($"Количество цифр в числе {number} равно {sum}");
+
+int NumCount(int num)
+{
+    int result=0;
+    while(num>0)
+    {
+        result=result+1;
+        num=num/10;
+    }
+    return result;
+}
