42f91592a376c1991c230e94252dcee00c74bbcf
Denis Levashov <denis.ne@e1.ru>
2023-01-19 21:24:34 +0500

    решение 32 задачи

---
+++ Task32/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+
+using static System.Console;
+Clear();
+
+int[] array = {-4, -8, 8, 2};
+resultArray(array);
+PrintArray(array);
+
+
+void resultArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        inArray[i]*=-1;
+    }
+    
+}
+
+
+
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]},");
+    }
+    WriteLine($"{inArray[inArray.Length - 1]}]");
+}
