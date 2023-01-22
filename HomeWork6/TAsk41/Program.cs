47776e1c5aaca54899a2a186e8fcf41ae519f4c3
Denis Levashov <denis.ne@e1.ru>
2023-01-22 23:38:33 +0500

    решение 41 задачи

---
+++ HomeWork6/TAsk41/Program.cs
@@ -0,0 +1,32 @@
+﻿////Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+using static System.Console;
+Clear();
+
+WriteLine("Введите целые числа через ПРОБЕЛ");
+int[] array = Array.ConvertAll(ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries), int.Parse);
+int count =0;
+int[] CreateArray()
+{
+    
+    
+    Write("[");
+
+    for (int i = 0; i < array.Length-1; ++i)
+    {
+        Write(array[i] + ", ");
+    }
+    WriteLine(array[array.Length - 1] + "]");
+    return array;
+}
+
+void PositiveNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]>0) count++;
+    }
+    WriteLine($"Пользователь ввел {count} чисел больше нуля");
+}
+PositiveNumbers(CreateArray());
