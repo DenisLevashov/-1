088476e97c83b4bb4643cdbd4f54b44aa8edea98
Denis Levashov <denis.ne@e1.ru>
2023-01-22 23:09:37 +0500

    коррекция решение 34 задачи

--- Homework5/Task34/Program.cs
+++ Homework5/Task34/Program.cs
@@ -25,13 +25,13 @@ int[] CreateArray()
     {
         array[i] = new Random().Next(100, 1000);
     }
-    Console.Write("[");
+    Write("[");
 
     for (int i = 0; i < array.Length-1; ++i)
     {
-        Console.Write(array[i] + ", ");
+        Write(array[i] + ", ");
     }
-    Console.WriteLine(array[array.Length - 1] + "]");
+    WriteLine(array[array.Length - 1] + "]");
     return array;
 }
 
